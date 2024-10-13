d91936d0bbe4296f4b1b89236c8bc10ab19220bb
ToviasNunez <toviasnunez@hotmail.com>
2024-10-13 19:00:57 +0200

    udemy video 61

---
+++ UdemyScalatoMaster/src/main/scala/Booleans.scala
@@ -0,0 +1,5 @@
+class Booleans:
+  
+
+end Booleans
+
---
+++ UdemyScalatoMaster/src/main/scala/OperatorAndBoolean.scala
@@ -0,0 +1,41 @@
+class OperatorAndBoolean(a: Double, b: Double):
+
+  // Arithmetic operator Additional + / -
+  def addition: Double =
+    val addition = a + b
+    addition
+
+  def subtraction: Double =
+    val subtractable = a - b
+    subtractable
+
+  def multiplication: Double =
+    val multi = a * b
+    multi
+
+  def division: Double =
+    val div = a / b
+    div
+
+  def modules: Double =
+    val mod = a % b
+    mod
+  // Result types
+  // Assignment operators
+
+  val dozen = 12
+  var month = dozen
+
+  // Booleans
+  // Logical operators
+
+  def augmentations: Int =
+    var bikes = 1
+    bikes
+
+  def calculatePercentOfYear(principal: Double, rate: Double, time: Int): Double =
+    val totalAmount = principal * Math.pow(1 + rate / 100, time)
+    totalAmount
+
+end OperatorAndBoolean
+
